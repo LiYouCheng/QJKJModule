bb272deb53ced3fdcdf00432fbff52ea147663fa
史ios <542235666@qq.com>
2016-12-28 22:01:07 +0800

    [基本模块】UITextView 增加占位符

---
+++ 控件基类/BaseView/UITextView/QJKJTextView.h
@@ -0,0 +1,23 @@
+//
+//  QJKJTextView.h
+//  LYCModuleManager
+//
+//  Created by LiYouCheng on 16/12/19.
+//  Copyright © 2016年 深圳市齐家互联网科技股份有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QJKJTextView : UITextView
+
+/**
+ *  默认文本
+ */
+@property (nonatomic, strong) NSString *placeholder;
+
+/**
+ *  默认文本颜色
+ */
+@property (nonatomic, strong) UIColor *placeholderColor;
+
+@end
